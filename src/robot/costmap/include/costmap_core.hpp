2c24aacbb50a6012d00a61fe9dc8732fa03f46ca
Aditya Ajay <adityaajay@Adityas-MacBook-Pro.local>
2025-01-19 23:51:33 -0500

    assignment complete - made final touch ups before submission

--- src/robot/costmap/include/costmap_core.hpp
+++ src/robot/costmap/include/costmap_core.hpp
@@ -2,20 +2,36 @@
 #define COSTMAP_CORE_HPP_
 
 #include "rclcpp/rclcpp.hpp"
+#include "nav_msgs/msg/occupancy_grid.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
 
-namespace robot
-{
+namespace robot {
 
 class CostmapCore {
-  public:
-    // Constructor, we pass in the node's RCLCPP logger to enable logging to terminal
-    explicit CostmapCore(const rclcpp::Logger& logger);
+public:
+  void initCostmap(
+    double resolution, 
+    int width, 
+    int height, 
+    geometry_msgs::msg::Pose origin, 
+    double inflation_radius
+  );
+  
+  explicit CostmapCore(const rclcpp::Logger& logger);
 
-  private:
-    rclcpp::Logger logger_;
+  void updateCostmap(const sensor_msgs::msg::LaserScan::SharedPtr laserscan) const;
+  void inflateObstacle(int origin_x, int origin_y) const;
 
+  nav_msgs::msg::OccupancyGrid::SharedPtr getCostmapData() const;
+
+private:
+  nav_msgs::msg::OccupancyGrid::SharedPtr costmap_data_;
+  rclcpp::Logger logger_;
+
+  double inflation_radius_;
+  int inflation_cells_;
 };
 
-}  
+}  // namespace robot
 
-#endif  
+#endif  // COSTMAP_CORE_HPP_
--- src/robot/map_memory/include/map_memory_core.hpp
+++ src/robot/map_memory/include/map_memory_core.hpp
@@ -1,19 +1,40 @@
-#ifndef MAP_MEMORY_CORE_HPP_
-#define MAP_MEMORY_CORE_HPP_
+#ifndef MAP_MEMORY_HANDLER_HPP_
+#define MAP_MEMORY_HANDLER_HPP_
 
-#include "rclcpp/rclcpp.hpp"
+#include <rclcpp/rclcpp.hpp>
+#include <nav_msgs/msg/occupancy_grid.hpp>
+#include <geometry_msgs/msg/pose.hpp>
 
-namespace robot
+namespace memory_ns
 {
 
-class MapMemoryCore {
-  public:
-    explicit MapMemoryCore(const rclcpp::Logger& logger);
+class MapMemoryHandler {
+public:
+  explicit MapMemoryHandler(const rclcpp::Logger & logger);
 
-  private:
-    rclcpp::Logger logger_;
+  void initializeMemory(
+    double resolution,
+    int width,
+    int height,
+    const geometry_msgs::msg::Pose & map_origin
+  );
+
+  void mergeLocalCostmap(
+    const nav_msgs::msg::OccupancyGrid::SharedPtr & local_costmap,
+    double robot_x, double robot_y, double robot_heading
+  );
+
+  // Convert world coordinates (rx, ry) into map indices (mx, my)
+  bool convertToMapIndices(double rx, double ry, int &mx, int &my);
+
+  // Retrieve the global occupancy grid
+  nav_msgs::msg::OccupancyGrid::SharedPtr retrieveMemoryGrid() const;
+
+private:
+  nav_msgs::msg::OccupancyGrid::SharedPtr global_memory_;  
+  rclcpp::Logger logger_;
 };
 
-}  
+}  // namespace memory_ns
 
-#endif  
+#endif  // MAP_MEMORY_HANDLER_HPP_
